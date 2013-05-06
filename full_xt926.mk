0268b978b382bab9aa6e72a06fc4a54c2f348425
pcarenza <pcarenza@gmail.com>
2013-05-06 14:25:04 -0400

    pull in verizon apns

--- full_xt926.mk
+++ full_xt926.mk
@@ -3,6 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/xt926/device_xt926.mk)
 
 ## Device identifier. This must come after all inclusions
+PRODUCT_COPY_FILES += device/sample.etc/apns/conf_verizon.xml:system/etc/apns-conf.xml
 PRODUCT_DEVICE := xt926
 PRODUCT_NAME := full_xt926
 PRODUCT_BRAND := motorola
